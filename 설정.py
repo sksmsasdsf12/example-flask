7c22bc370e8eaed92ac39f4b51aa878eea3d33f7
sksmsasdsf12 <159116371+sksmsasdsf12@users.noreply.github.com>
2024-03-01 14:57:29 +0900

    Update and rename app.py to 설정.py

---
+++ 설정.py
@@ -0,0 +1,17 @@
+봇_토큰 = "MTE4NTQzMDYzODM0NjY0MTQyOA.GrmDb9.Tfr2L6ZLHPYKXKC2WN-UHIM5BQUyX-mxrtLN6Q" #봇의 토큰
+관리자아이디 = [938641141958602772 , 939420271553691688 , 737056107909873676] #라이센스 뽑을 수 있는 사람의 아이디
+API_엔드포인트 = "https://discord.com/api/v9"  #웬만해선 안건드려도됨
+클라이언트_아이디 = "1185430638346641428" #디스코드 개발자 센터 Oauth2 탭에 들어가면 있는 Client ID
+클라이언트_시크릿 = "z0evudsVAV3IHhqyds1re6u9J3YA7lLI" #디스코드 개발자 센터 Oauth2 탭에 들어가면 있는 Client Secret
+도메인 = "https://sksmsasdsf.store" #복구봇을 이용할 도메인
+복구로그웹훅 = "https://discord.com/api/webhooks/1206840243559338015/VIT6o_LbJsweN9ERkOdyx-vBwq5drlWJ1qWfHIXXOrXYu9y-wC1p7nbA7OeI61ZMwzIs"
+
+########### 건드리지 마세요 ###########
+token = 봇_토큰
+admin_id = 관리자아이디
+api_endpoint = API_엔드포인트
+client_id = 클라이언트_아이디
+client_secret = 클라이언트_시크릿
+base_url = 도메인
+bokweb =복구로그웹훅
+########### 건드리지 마세요 ###########
